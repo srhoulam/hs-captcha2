818f0e8f9b35fe09f06e9a9b9ddb1a1414458f1b
Saad Rhoulam <saad@rhoulam.com>
2022-08-30 19:45:19 -0400

    Version 2.0
    - Substantially improve captcha legibility.
      - Use base58 alphabet to eliminate look-alikes.
      - Use configuration object to make captcha customizable.
        - Default: chirp only vertically. This produces very wavy, but also very readable captchas.
    - Add themes, striping, customizability.
      - Chirp parameters are specified by config object.
      - Dark/light themes for basic captcha.
      - Striped themes for more advanced captchas. Vertical and horizontal
        striping supported.
      - Refactor chirping, which was not very readable.
    - Export everything.

--- Graphics/Captcha.hs
+++ Graphics/Captcha.hs
@@ -1,149 +1,225 @@
-module Graphics.Captcha (makeCaptcha) where
+module Graphics.Captcha where
 
-import Data.ByteString
-import Data.Char
-import Graphics.GD
-import System.Random
+import           Control.Monad
+import           Data.ByteString (ByteString)
+import           Data.Char
+import           GHC.Arr
+import           Graphics.GD
+import           System.Random
 
+data Theme = Light | Dark | StripedHoriz | StripedVert
+
+data CaptchaConfig = CaptchaConfig
+  { ccChirpHorizontally :: Bool
+  , ccChirpVertically   :: Bool
+  , ccTheme             :: Theme
+  , ccStripes           :: Int
+  , ccMinChirpDepth     :: Float
+  , ccMaxChirpDepth     :: Float
+  , ccFontName          :: String
+  , ccFontSize          :: Double
+  , ccCaptchaSize       :: Int
+  , ccFinalSize         :: (Int, Int)
+  }
+
+defaultConfig = CaptchaConfig
+  { ccChirpHorizontally = False
+  , ccChirpVertically = True
+  , ccTheme = StripedHoriz
+  , ccStripes = 2
+  , ccMinChirpDepth = 1.0
+  , ccMaxChirpDepth = 2.5
+  , ccFontName = "Courier New"
+  , ccFontSize = 22.0
+  , ccCaptchaSize = 192
+  , ccFinalSize = (128, 64)
+  }
+
+alphabet :: Array Int Char
+alphabet = listArray (0, 57) "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 
 main :: IO ()
 main = do
+  let cfg = defaultConfig
   string <- makeRandomString
-  image <- createInitialImage string
-  chirpDoubleRandom image
-  chirpDoubleRandom image
-  image <- cropToFinalSize image
-  Prelude.putStrLn string
+  image <- createInitialImage cfg string
+  chirpDoubleRandom cfg image
+  chirpDoubleRandom cfg image
+  image <- cropToFinalSize cfg image
+  putStrLn string
   savePngFile "captcha.png" image
 
 
-makeCaptcha :: IO (String, ByteString)
-makeCaptcha = do
+makeCaptcha :: CaptchaConfig -> IO (String, ByteString)
+makeCaptcha cfg = do
   string <- makeRandomString
-  image <- createInitialImage string
-  chirpDoubleRandom image
-  chirpDoubleRandom image
-  image <- cropToFinalSize image
+  image <- createInitialImage cfg string
+  chirpDoubleRandom cfg image
+  chirpDoubleRandom cfg image
+  image <- cropToFinalSize cfg image
   byteString <- savePngByteString image
   return (string, byteString)
 
 
 makeRandomString :: IO String
-makeRandomString = do
-  let makeRandomLetter = do
-                     n <- randomRIO (0, 25)
-                     return $ chr (ord 'A' + n)
-  mapM (\_ -> makeRandomLetter) [0..5]
+makeRandomString = mapM (const makeRandomLetter) [0..5]
+  where makeRandomLetter = do
+          n <- randomRIO (0, 57)
+          return $ alphabet ! n
 
 
-chirpDoubleRandom :: Image -> IO ()
-chirpDoubleRandom image = do
-  depth1 <- randomRIO (2.0, 5.0)
-  period1 <- randomRIO (1, 6)
-  period2 <- randomRIO (1, 6)
-  depth2 <- randomRIO (2.0, 5.0)
-  period3 <- randomRIO (1, 6)
-  period4 <- randomRIO (1, 6)
-  chirpVertically (makeChirpFunction depth1
-                                     (fromIntegral captchaSize / period1)
-                                     (fromIntegral captchaSize / period2))
-                  image
-  chirpHorizontally (makeChirpFunction depth2
-                                       (fromIntegral captchaSize / period3)
-                                       (fromIntegral captchaSize / period4))
-                    image
+chirpDoubleRandom :: CaptchaConfig -> Image -> IO ()
+chirpDoubleRandom cfg image = do
+  when (ccChirpVertically cfg) $ do
+    depth1 <- randomRIO (ccMinChirpDepth cfg, ccMaxChirpDepth cfg)
+    period1 <- randomRIO (1, 6)
+    period2 <- randomRIO (1, 6)
+    let captchaSizeF = fromIntegral $ ccCaptchaSize cfg
+    chirpVertically
+      (ccCaptchaSize cfg)
+      (makeChirpFunction
+        (ccCaptchaSize cfg)
+        depth1
+        (captchaSizeF / period1)
+        (captchaSizeF / period2))
+      image
+
+  when (ccChirpHorizontally cfg) $ do
+    depth2 <- randomRIO (ccMinChirpDepth cfg, ccMaxChirpDepth cfg)
+    period3 <- randomRIO (1, 6)
+    period4 <- randomRIO (1, 6)
+    let captchaSizeF = fromIntegral $ ccCaptchaSize cfg
+    chirpHorizontally
+      (ccCaptchaSize cfg)
+      (makeChirpFunction
+        (ccCaptchaSize cfg)
+        depth2
+        (captchaSizeF / period3)
+        (captchaSizeF / period4))
+      image
 
 
-chirpHorizontally :: (Int -> Int) -> Image -> IO ()
-chirpHorizontally chirpFunction image = do
+chirpHorizontally :: Int -> (Int -> Int) -> Image -> IO ()
+chirpHorizontally captchaSize chirpFunction image = do
   withImage (newImage (captchaSize, captchaSize))
-            (\temporaryImage -> do
-               copyRegion (0, 0) (captchaSize, captchaSize) image
-                          (0, 0) temporaryImage
-               let shiftRow row amount
-                       = copyRegion (0, row) (captchaSize, 1) image
-                                    (amount, row) temporaryImage
-                   shiftRows function
-                       = shiftRows' 0 function
-                   shiftRows' i function
-                       = if i == captchaSize
-                            then return ()
-                            else do
-                              shiftRow i (function i)
-                              shiftRows' (i+1) function
-               shiftRows chirpFunction
-               copyRegion (0, 0) (captchaSize, captchaSize) temporaryImage
-                          (0, 0) image)
+    (\temporaryImage -> do
+        copyRegion
+          (0, 0)
+          (captchaSize, captchaSize)
+          image
+          (0, 0)
+          temporaryImage
+
+        forM_ [0 .. captchaSize - 1] $ \i -> copyRegion
+          (0, i)
+          (captchaSize, 1)
+          image
+          (chirpFunction i, i)
+          temporaryImage
+
+        copyRegion
+          (0, 0)
+          (captchaSize, captchaSize)
+          temporaryImage
+          (0, 0)
+          image)
 
 
-chirpVertically :: (Int -> Int) -> Image -> IO ()
-chirpVertically chirpFunction image = do
+chirpVertically :: Int -> (Int -> Int) -> Image -> IO ()
+chirpVertically captchaSize chirpFunction image = do
   withImage (newImage (captchaSize, captchaSize))
-            (\temporaryImage -> do
-               copyRegion (0, 0) (captchaSize, captchaSize) image
-                          (0, 0) temporaryImage
-               let shiftColumn column amount
-                       = copyRegion (column, 0) (1, captchaSize) image
-                                    (column, amount) temporaryImage
-                   shiftColumns function
-                       = shiftColumns' 0 function
-                   shiftColumns' i function
-                       = if i == captchaSize
-                            then return ()
-                            else do
-                              shiftColumn i (function i)
-                              shiftColumns' (i+1) function
-               shiftColumns chirpFunction
-               copyRegion (0, 0) (captchaSize, captchaSize) temporaryImage
-                          (0, 0) image)
+    (\temporaryImage -> do
+        copyRegion
+          (0, 0)
+          (captchaSize, captchaSize)
+          image
+          (0, 0)
+          temporaryImage
+
+        forM_ [0 .. captchaSize - 1] $ \i -> copyRegion
+          (i, 0)
+          (1, captchaSize)
+          image
+          (i, chirpFunction i)
+          temporaryImage
+
+        copyRegion
+          (0, 0)
+          (captchaSize, captchaSize)
+          temporaryImage
+          (0, 0)
+          image)
 
 
-makeChirpFunction :: Float -> Float -> Float -> Int -> Int
-makeChirpFunction depth startingWaveLength endingWaveLength row
-    = let waveLength = ((startingWaveLength * fromIntegral row)
-                        + (endingWaveLength * fromIntegral (captchaSize - row)))
-                       / fromIntegral captchaSize
-      in floor $ depth * (sin $ (fromIntegral row) * ((2*pi) / waveLength))
+makeChirpFunction :: Int -> Float -> Float -> Float -> Int -> Int
+makeChirpFunction captchaSize depth startingWaveLength endingWaveLength row =
+  let waveLength = ((startingWaveLength * fromIntegral row)
+                     + (endingWaveLength * fromIntegral (captchaSize - row)))
+                   / fromIntegral captchaSize
+  in floor $ depth * sin (fromIntegral row * ((2*pi) / waveLength))
 
 
-createInitialImage :: String -> IO Image
-createInitialImage string = do
+createInitialImage :: CaptchaConfig -> String -> IO Image
+createInitialImage cfg = case ccTheme cfg of
+  Dark         -> createInitialImageSimple True cfg
+  Light        -> createInitialImageSimple False cfg
+  StripedHoriz -> createInitialImageStriped False cfg
+  StripedVert  -> createInitialImageStriped True cfg
+
+createInitialImageStriped :: Bool -> CaptchaConfig -> String -> IO Image
+createInitialImageStriped vert cfg string = do
+  darkImage <- createInitialImageSimple True cfg string
+  lightImage <- createInitialImageSimple False cfg string
+  let pxPerStripe = floor $ fromIntegral (ccCaptchaSize cfg) / fromIntegral (ccStripes cfg)
+      captchaSize = ccCaptchaSize cfg
+
+  if vert then forM_ (filter even [0 .. ccStripes cfg - 1]) $ \stripe ->
+    copyRegion
+      (stripe * pxPerStripe, 0)
+      (pxPerStripe, captchaSize)
+      lightImage
+      (stripe * pxPerStripe, 0)
+      darkImage
+    else forM_ (filter even [0 .. ccStripes cfg - 1]) $ \stripe ->
+      copyRegion
+        (0, stripe * pxPerStripe)
+        (captchaSize, pxPerStripe)
+        lightImage
+        (0, stripe * pxPerStripe)
+        darkImage
+
+  return darkImage
+
+createInitialImageSimple :: Bool -> CaptchaConfig -> String -> IO Image
+createInitialImageSimple dark cfg string = do
+  let captchaSize = ccCaptchaSize cfg
+      fontName = ccFontName cfg
+      fontSize = ccFontSize cfg
+      foregroundColor = if dark then 0xFFFFFF else 0x000000
+      backgroundColor = if dark then 0x000000 else 0xFFFFFF
   image <- newImage (captchaSize, captchaSize)
   useFontConfig True
-  drawFilledRectangle (0, 0) (captchaSize, captchaSize) 0xFFFFFF image
+  drawFilledRectangle (0, 0) (captchaSize, captchaSize) backgroundColor image
   ((left, top), _, (right, bottom), _)
-      <- measureString fontName fontSize 0.0 (0, 0) string 0x000000
-  width <- return $ right - left
-  height <- return $ top - bottom
-  originX <- return $ (captchaSize - width) `div` 2
-  originY <- return $ (captchaSize + height) `div` 2
-  drawString fontName fontSize 0.0 (originX, originY) string 0x000000 image
+      <- measureString fontName fontSize 0.0 (0, 0) string foregroundColor
+
+  let width = right - left
+      height = top - bottom
+      originX = (captchaSize - width) `div` 2
+      originY = (captchaSize + height) `div` 2
+  drawString fontName fontSize 0.0 (originX, originY) string foregroundColor image
   return image
 
 
-cropToFinalSize :: Image -> IO Image
-cropToFinalSize image = do
+cropToFinalSize :: CaptchaConfig -> Image -> IO Image
+cropToFinalSize cfg image = do
+  let captchaSize = ccCaptchaSize cfg
+      finalSize = ccFinalSize cfg
   result <- newImage finalSize
-  copyRegion ((captchaSize - fst finalSize) `div` 2,
-              (captchaSize - snd finalSize) `div` 2)
+  copyRegion ( (captchaSize - fst finalSize) `div` 2
+             , (captchaSize - snd finalSize) `div` 2)
              finalSize
              image
              (0, 0)
              result
   return result
-
-
-fontName :: String
-fontName = "Courier New"
-
-
-fontSize :: Double
-fontSize = 22.0
-
-
-captchaSize :: Int
-captchaSize = 192
-
-
-finalSize :: (Int, Int)
-finalSize = (128, 64)
